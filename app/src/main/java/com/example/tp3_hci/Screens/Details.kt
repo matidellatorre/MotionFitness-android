eb1c5614299f30488d11701be5a01ae9ca2a3da2
gastialasia <86937493+gastialasia@users.noreply.github.com>
2022-11-11 12:13:18 -0300

    Update Details.kt

--- app/src/main/java/com/example/tp3_hci/Screens/Details.kt
+++ app/src/main/java/com/example/tp3_hci/Screens/Details.kt
@@ -1,16 +1,16 @@
 package com.example.tp3_hci.Screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Divider
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tp3_hci.R
 import com.example.tp3_hci.components.CicleEntry
 import com.example.tp3_hci.components.MainAppBar
@@ -22,15 +22,24 @@ fun DetailsScreen(id: Int) {
             MainAppBar(title = stringResource(id = R.string.details_top_text), hasAvatar = false, hasSearch = false)
         }
     ) {
-        LazyColumn (
-            verticalArrangement = Arrangement.SpaceEvenly,
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            items(100) {
-                CicleEntry(title = "Ciclo 1", exerciseCount = 5)
-                //Divider(thickness = 2.dp)
+        Column() {
+            Text(
+                text = "This are your cycles",
+                fontSize = 22.sp,
+                fontWeight = FontWeight(500),
+                modifier = Modifier.padding(8.dp),
+            )
+            LazyColumn (
+                verticalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                items(100) {
+                    CicleEntry(title = "Ciclo 1", exerciseCount = 5)
+                    //Divider(thickness = 2.dp)
+                }
             }
         }
+
     }
 }
