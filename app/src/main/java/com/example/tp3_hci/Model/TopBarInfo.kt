50d7705727f7e9bcb9f4ac40e675b44995cea04c
gastialasia <86937493+gastialasia@users.noreply.github.com>
2022-11-11 19:19:23 -0300

    Top bar info elevated successfully & backArrow implemented
    Co-Authored-By: Matías Della Torre <72523405+matidellatorre@users.noreply.github.com>

---
+++ app/src/main/java/com/example/tp3_hci/Model/TopBarInfo.kt
@@ -0,0 +1,7 @@
+package com.example.tp3_hci.Model
+
+class TopBarInfo (
+    val title: String,
+    val hasAvatar: Boolean,
+    val hasSearch: Boolean,
+    val hasBackArrow: Boolean)
--- app/src/main/java/com/example/tp3_hci/components/MainAppBar.kt
+++ app/src/main/java/com/example/tp3_hci/components/MainAppBar.kt
@@ -3,21 +3,31 @@ package com.example.tp3_hci.components
 import android.content.Context
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.tp3_hci.Model.BottomIcon
+import com.example.tp3_hci.Model.TopBarInfo
 import com.example.tp3_hci.R
 
 @Composable
 fun MainAppBar(navController: NavHostController) {
     val backStackEntry by navController.currentBackStackEntryAsState()
+    var currentRoute = backStackEntry?.destination?.route?: "home"
+    var topBarInfoMap by remember { mutableStateOf( hashMapOf<String, TopBarInfo>(
+        "home" to TopBarInfo("Home", true, false, false),
+        "routines" to TopBarInfo("Routines", false, true, false),
+        "explore" to TopBarInfo("Explore", false, false, false),
+        "details/1" to TopBarInfo("Details", false, true, true),
+            )
+        )
+    }
+
     TopAppBar(
-        title = { Text(text = backStackEntry?.destination?.route ?: "Home", fontSize = 28.sp) },
+        title = { Text(text = topBarInfoMap.get(currentRoute)!!.title, fontSize = 28.sp) },
         backgroundColor = MaterialTheme.colors.primary,
         actions = {
 //            if (hasSearch) {
@@ -31,5 +41,15 @@ fun MainAppBar(navController: NavHostController) {
 //                //Avatar
 //            }
         },
+        navigationIcon = if (topBarInfoMap.get(currentRoute)!!.hasBackArrow) {
+            {
+                IconButton(onClick = { navController.navigateUp() }) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "Back"
+                    )
+                }
+            }
+        } else {null},
     )
 }
