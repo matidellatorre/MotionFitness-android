26a67c48352dd00d1f7db4f98b0760e9192d6eec
gastialasia <86937493+gastialasia@users.noreply.github.com>
2022-11-12 18:40:20 -0300

    Bottom bar remove from execution

--- app/src/main/java/com/example/tp3_hci/components/MainBottomBar.kt
+++ app/src/main/java/com/example/tp3_hci/components/MainBottomBar.kt
@@ -14,34 +14,39 @@ import com.example.tp3_hci.R
 
 @Composable
 fun MainBottomBar(navController: NavController) {
-    val navBackStackEntry by navController.currentBackStackEntryAsState()
-    val currentRoute = navBackStackEntry?.destination?.route
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = backStackEntry?.destination?.route?: "home"
     val items = listOf(
         BottomIcon(Icons.Default.Home, stringResource(id = R.string.home_top_text),"home"),
         BottomIcon(Icons.Default.FitnessCenter, stringResource(id = R.string.routines_top_text),"routines"),
-        BottomIcon(Icons.Default.Public, stringResource(id = R.string.explore_top_text),"explore")
+        BottomIcon(Icons.Default.Public, stringResource(id = R.string.explore_top_text),"explore"),
+    )
+    val screensBlocked = listOf(
+        "execution/1"
     )
 
-    BottomNavigation (
-        backgroundColor = MaterialTheme.colors.primary
-    ) {
-        items.forEachIndexed { index, item ->
-            BottomNavigationItem(
-                icon = { Icon(item.imageVector, contentDescription = null) },
-                label = { Text(item.label) },
-                selected = currentRoute == item.route,
-                onClick = {
-                    navController.navigate(item.route) {
-                        navController.graph.startDestinationRoute?.let { screenRoute ->
-                            popUpTo(screenRoute) {
-                                saveState = true
+    if(!screensBlocked.contains(currentRoute)) {
+        BottomNavigation (
+            backgroundColor = MaterialTheme.colors.primary
+        ) {
+            items.forEachIndexed { index, item ->
+                BottomNavigationItem(
+                    icon = { Icon(item.imageVector, contentDescription = null) },
+                    label = { Text(item.label) },
+                    selected = currentRoute == item.route,
+                    onClick = {
+                        navController.navigate(item.route) {
+                            navController.graph.startDestinationRoute?.let { screenRoute ->
+                                popUpTo(screenRoute) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
                             }
-                            launchSingleTop = true
-                            restoreState = true
                         }
                     }
-                }
-            )
+                )
+            }
         }
     }
 }
