6bba83f9b09d6d02fb95d7fc91fea6de468d1550
gastialasia <86937493+gastialasia@users.noreply.github.com>
2022-11-18 11:42:32 -0300

    Centered empty state msg in routine card list

--- app/src/main/java/com/example/tp3_hci/components/RoutineCardList.kt
+++ app/src/main/java/com/example/tp3_hci/components/RoutineCardList.kt
@@ -1,11 +1,14 @@
 package com.example.tp3_hci.components
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.HideSource
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -22,7 +25,12 @@ fun RoutineCardList(
     onNavigateToExecution: (id:Int) -> Unit,
 ) {
     if (list==null || list.isEmpty()){
-        EmptyState(text = stringResource(id = R.string.empty_routines), imgVector = Icons.Default.HideSource)
+        Box (
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
+                ){
+            EmptyState(text = stringResource(id = R.string.empty_routines), imgVector = Icons.Default.HideSource)
+        }
     } else {
         LazyColumn(
             verticalArrangement = Arrangement.spacedBy(15.dp),
